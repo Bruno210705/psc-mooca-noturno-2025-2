9ab50ec54d6f2bf3450c47bf9c66bb1365f79812
Bruno Henrique Almeida Santos <825142649@ulife.com.br>
2025-09-16 21:03:17 -0300

    aula 3 poo

--- demo/src/main/java/com/example/Jogo.java
+++ demo/src/main/java/com/example/Jogo.java
@@ -4,9 +4,9 @@ package com.example;
 public class Jogo {
 
     public static void main(String[] args) {
-        Personagem cacador = new Personagem(1, 1, 1);
-        //Personagem soneca = new Personagem();
-        cacador.nome = "João";
+        Personagem cacador = new Personagem("Diniz",1, 1, 1);
+        Personagem soneca = new Personagem("Hugo",2, 2, 2);
+        //cacador.nome = "João";
     //soneca.nome = "Danilo";
 
 
